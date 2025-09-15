46cc74280cfbdf3d5fe9e62afaec2cca5769f341
jhemder <kevindj13579@gmail.com>
2025-09-15 05:15:04 -0500

    el juego en godot bro

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _on_play_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_umas_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_shop_button_down() -> void:
+	pass # Replace with function body.
+
+func _on_exit_button_down() -> void:
+	get_tree().quit()
